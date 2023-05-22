89b71c294e2bc88a787a56c004a6587f5ddf07e1
helen121027 <helen121027@gmail.com>
2023-05-23 01:24:28 +0900

    Feat: 상품 더미데이터

---
+++ src/data/ProductsList.js
@@ -0,0 +1,54 @@
+const productsList = [
+  {
+    productId: 'TMT3110503',
+    img: require('../images/1-1.jpg').default,
+    brandName: '10MONTH',
+    productName: '변형소매 골드버튼 자켓-3부',
+    price: '143,280',
+    color: {
+      ivory: '#F9F5EA',
+      black: '#000000'
+    },
+    size: ['S', 'M'],
+    deliveryFee: '무료'
+  },
+  {
+    productId: 'TMT3111501',
+    img: '',
+    brandName: '10MONTH',
+    productName: 'V넥 변형 소매 셔츠-3부',
+    price: '85,680',
+    color: {
+      white: '#FFFFFF',
+      pink: '#FFC0CB'
+    },
+    size: ['S', 'M'],
+    deliveryFee: '무료'
+  },
+  {
+    productId: 'TMT3113502',
+    img: '',
+    brandName: '10MONTH',
+    productName: 'Summer Kint_비대칭넥 가디건_3부',
+    price: '75,730',
+    color: {
+      black: '#000000',
+      lightBeige: '#F5DEB3'
+    },
+    size: ['S', 'M'],
+    deliveryFee: '무료'
+  },{
+    productId: 'TMT3153400',
+    img: '',
+    brandName: '10MONTH',
+    productName: '10;Kijun_카라 집업 가이던',
+    price: '121,630',
+    color: {
+      lime: '#00FF00',
+      black: '#000000'
+    },
+    size: ['S', 'M'],
+    deliveryFee: '무료'
+  },
+]
+export default productsList;
