1567d8dd9ff445e40976e92eeb26759348a14955
helen121027 <helen121027@gmail.com>
2023-05-22 15:15:57 +0900

    Feat: sidebar 틀 잡기

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,35 @@
+import { Fragment } from "react";
+
+const Sidebar = () => {
+  const categories = [
+    { name: '전체'},
+    { name: '세일'},
+    { name: '아우터'},
+    { name: '블라우스/셔츠'},
+    { name: '티셔츠'},
+    { name: '맨투맨/후디'},
+    { name: '니트'},
+    { name: '팬츠'},
+    { name: '데님'},
+    { name: '드레스/점프수트'},
+    { name: '수트/셋업'},
+    { name: '라운지/파자마'},
+    { name: '언더웨어'},
+  ]
+  return(
+    <Fragment>
+      <div className="mb-12">
+        <p className="text-[28px] font-bold">여성의류</p>
+      </div>
+      <div>
+        {categories.map((category, index) => {
+          return (
+            <p className="text-[16px] my-5 hover:cursor-pointer hover:underline text-gray-600">{category.name}</p>
+          )
+        })}
+      </div>
+    </Fragment>
+
+  )
+}
+export default Sidebar;
