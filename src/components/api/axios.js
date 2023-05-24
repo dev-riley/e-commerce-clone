c04ec9ca3a324b2a32cfd648b25dd9e3134e3f57
helen121027 <helen121027@gmail.com>
2023-05-24 17:02:52 +0900

    Comment: axios 개별 api 설명 주석 추가

--- src/components/api/axios.js
+++ src/components/api/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+// 상품 전체 목록 조회
 const getProductList = async () => {
   console.log("성공")
   return await axios({
@@ -8,6 +9,7 @@ const getProductList = async () => {
   })
 }
 
+// 상품 상세 정보 조회
 const getProductDetail = async(id) => {
   console.log('getProductDetail 성공')
   return await axios({
@@ -15,5 +17,8 @@ const getProductDetail = async(id) => {
     url: `http://localhost:8000/products/${id}`
   })
 }
+
+// 상품 등록
+
 
 export { getProductList, getProductDetail }
