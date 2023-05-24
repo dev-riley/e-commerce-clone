56f9afe17be4eb7c1f22747e03c951ab4dd2cae4
helen121027 <helen121027@gmail.com>
2023-05-24 10:20:19 +0900

    Feat : 상품 목록 api연결

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,10 +1,17 @@
-import productsList from '../data/ProductsList';
+import { useEffect, useState } from 'react';
 import CardItem from './CardItem';
+import {getProductList} from './api/axios';
 
 const CardList = () => {
+  const [list, SetList] = useState([])
+
+  useEffect(() => {
+    getProductList().then((res) => SetList(res.data))
+  }, [])
+  // console.log(list)
   return(
     <div className='flex flex-wrap mt-3'>
-      {productsList.map(product => (
+      {list.map(product => (
         <CardItem key={product.productId} product={product} />
       ))}
     </div>
