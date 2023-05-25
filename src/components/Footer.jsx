47910145b7a18f74c73cce9883d4716bccde4272
helen121027 <helen121027@gmail.com>
2023-05-26 08:08:58 +0900

    Common : footer div 태그 추가

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,8 @@
 const Footer = () => {
   return(
-    <div className="h-[200px] w-full bg-gray-200 mt-[100px]"></div>
+    <div>
+      <div className="h-[200px] w-full bg-gray-200 mt-[100px] bottom-0"></div>
+    </div>
   )
 }
 export default Footer;
