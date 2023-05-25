ff9cda3a0dcc1747b2bf6cd88350b8f6d1be9ab2
helen121027 <helen121027@gmail.com>
2023-05-26 07:55:14 +0900

    Feat : 숫자 콤마 추가

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -7,7 +7,7 @@ const CardItem = ({product}) => {
   const toDetailPage = () => {
     navigate(`/products/${id}`)
   }
-  
+  console.log(img)
   return(
     <div className="p-4 md:s-1/4 mb-[130px] cursor-pointer" onClick={toDetailPage}>
       <div>
@@ -17,7 +17,7 @@ const CardItem = ({product}) => {
         <p className="opacity-3 font-bold ml-2 mt-4 mb-2">{brandName}</p>
         <div className="text-sm text-gray-900/75 ml-2">
           <p className="hover:underline">{productName}</p>
-          <p>{price}</p>
+          <p>{price.toLocaleString('ko-KR')}</p>
         </div>
       </div>
     </div>
